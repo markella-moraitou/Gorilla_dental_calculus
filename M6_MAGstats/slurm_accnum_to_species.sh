51bed84c61c81ab9d6007dac24d66731c2b4a58c
Markella Moraitou <markella.moraitou.0437@student.uu.se>
2021-10-27 13:41:06 +0200

    An attempt to parallelize the script

--- M6_MAGstats/slurm_accnum_to_species.sh
+++ M6_MAGstats/slurm_accnum_to_species.sh
@@ -16,19 +16,27 @@ echo $(module list)
 
 summary_file=/proj/sllstore2017021/nobackup/MARKELLA/RD2_mapping/assembly_summary_bacteria.txt
 
-#touch species_names.txt
+touch species_names.txt
+
+#Keep count so that the number of jobs running in parallel does not exceed the threshold
+
+count=0
+threshold=50
 
 cat mag_tree_accnum.txt | while read j
 do
     i=$(echo $j | tr -d '\r')
     #Check if the accession number already exists
     grep -q $i species_names.txt && continue
-    #Check if either the accession number ($1) or the corresponding RefSeq accession number ($18) match
+    #Check if either the accession number ($1) or the corresponding RefSeq accession number ($18) match, while incrementing the count variable
+    count=$(expr $count + 1)
     awk -vLOOKUPVAL=$i -v OFS='\t' '
     BEGIN {FS="\t"}; $1 == LOOKUPVAL || $18 == LOOKUPVAL {print LOOKUPVAL, $8}' < $summary_file >> species_names.txt &
-    #Wait once more than 5 jobs are running
-    if [[ $(jobs -p | grep "^[0-9]" | wc -l) > 5 ]]
+    #Wait once more than 50 jobs are running
+    echo "jobs $(jobs -p | grep "^[0-9]" | wc -l)"
+    if [[ $(jobs -p | grep "^[0-9]" | wc -l) -gt $threshold ]]
     then
-        wait
+        echo "waiting"
+        wait $(jobs -p | head -1)
     fi
 done
